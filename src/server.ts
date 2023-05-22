b786708ae60fd5065eb586fd1c95799ff75e052a
Natan Alonso <77931510+Natan-Alonso@users.noreply.github.com>
2023-05-22 16:44:58 -0300

    aula 3

--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,29 @@
+import "dotenv/config";
+
 import fastify from "fastify";
 import cors from "@fastify/cors";
+import jwt from "@fastify/jwt";
 import { memoriesRoutes } from "./routes/memories";
+import { authRoutes } from "./routes/auth";
 
 const app = fastify();
 // HTTP Method: GET, POST, PUT, PATCH, DELETE
 
+app.register(authRoutes);
 app.register(memoriesRoutes);
 
 app.register(cors, {
   origin: true,
 });
 
+app.register(jwt, {
+  secret: "spacetime",
+});
+
 app
   .listen({
     port: 3333,
+    host: "0.0.0.0",
   })
   .then(() => {
     console.log("🚀 HTTP server running on http://localhost:3333");
